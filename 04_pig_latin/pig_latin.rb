64e0bd755ef2050bc15f38c684f5e6d4855599ee
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-10-24 00:48:14 -0400

    fix pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,26 @@
 #write your code here
+
+def translate(str)
+    string = ""
+str.split(" ").each do |word| 
+    if word[0] =~ /[aeiou]/
+        string += word+"ay"+" "
+    else
+        tempString=""
+        while word[0] =~ /[^aeiou]/  do
+            if word[0] == "q" && word[1]=="u"
+             cons = word.slice!(0,2)  
+            else 
+                cons = word.slice!(0)
+            end
+            puts cons
+        tempString = word + cons 
+        word = tempString
+        tempString += "ay"+" "
+       
+        end
+         string+=tempString
+    end
+end
+   return string.strip
+end
