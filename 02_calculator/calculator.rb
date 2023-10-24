c9fe4d74ba29ee11ba0a530feff6dca9ca6d5d11
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-10-23 20:48:00 -0400

    fix calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,40 @@
 #write your code here
+# include "enumerable"
+def add(x,y)
+    x+y
+end
+
+def subtract(x,y)
+    x-y
+end
+
+def sum(arr)
+
+    if (arr != [])
+
+        # result = arr.each{|x| result*=x}
+        # puts result
+         arr.reduce{ |sum, n| sum + n }  
+    else 
+        arr = 0
+    #     result = 0
+    end
+    # puts result
+end
+
+def multiply (arr)
+    arr.reduce(1, :*)   
+end
+
+def power (x,y)
+    x**y 
+end
+
+def factorial(num)
+    if num!=0
+(1..num).reduce(1, :*) 
+    else 
+       return 1 
+    end
+end
+# puts sum([2,3,5])
