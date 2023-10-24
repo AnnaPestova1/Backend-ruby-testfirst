756acdfef5d4e692e016d49cb9ddea81c37d2d76
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-10-23 23:23:31 -0400

    fix simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,40 @@
 #write your code here
+
+def echo(str)
+ str
+end
+
+def shout (str)
+    str.upcase
+end
+
+def repeat(str, num=nil)
+    if num != nil
+       rep= (str+" ")*(num-1)+str
+    else
+       rep = str+" "+str
+    return rep
+    end
+ end 
+
+ def start_of_word(str, num)
+  str[0..num-1]
+ end
+
+ def first_word(str)
+    str.split(" ")[0]
+ end
+
+ def titleize(str)
+    string = ""
+   str.split(" ").each_with_index do |word, index| 
+    if index == 0
+        string += word[0].upcase + word[1, word.length-1]+" "
+    elsif word == "and" || word == "the" || word == "over"
+         string+=word + " "
+   else
+        string += word[0].upcase + word[1, word.length-1]+" "
+    end
+   end
+   return string.strip
+ end
